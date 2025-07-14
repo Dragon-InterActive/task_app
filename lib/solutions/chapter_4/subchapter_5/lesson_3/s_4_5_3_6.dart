ed7cee68423f24dbf27a2f03d94e2864c1e8a190
MasterNemo <info@dragon-software.net>
2025-07-14 16:46:28 +0200

    Task 4.5.3.6 solved

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,58 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    //Liste erstellen
+    final itemList = List<Product>.generate(
+        10, (i) => Product(name: 'Produkt ${i + 1}', price: 0.99 + i));
+    //Rückgabe des Widget
+    return ShoppingList(items: itemList);
+  }
+}
+
+//Produkt-Klasse
+class Product {
+  final String name;
+  final double price;
+
+  const Product({required this.name, required this.price});
+}
+
+//ListView - ShoppingList
+class ShoppingList extends StatelessWidget {
+  final List<Product> items;
+
+  const ShoppingList({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        String price = items[index].price.toString();
+        return ListTile(
+          title: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(items[index].name),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('$price €'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          leading: const Icon(Icons.shopping_cart),
+        );
+      },
+    );
   }
 }
